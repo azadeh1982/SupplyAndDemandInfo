623748cc64b4925537f277510d54b05029fc9206
Jnser <miracle868l@163.com>
2018-03-16 21:21:21 +0800

    html静态文件 utf-8

--- src/com/wust/util/DBUtil.java
+++ src/com/wust/util/DBUtil.java
@@ -69,7 +69,7 @@ public class DBUtil {
     }
 
     public static void main(String[] args){
-//        System.out.println(getConnection());
+        System.out.println(getConnection());
         System.out.println(Url);
     }
 }
